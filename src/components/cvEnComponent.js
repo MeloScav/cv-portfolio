be8044a6f0dac70cb5d8fcafb965838db18c645b
Méloé <meloscavezzoni@gmail.com>
2021-01-14 14:04:02 +0100

    French CV

--- src/components/cvEnComponent.js
+++ src/components/cvEnComponent.js
@@ -200,7 +200,7 @@ const CvEnComponent = (props) => {
       </div>
       <div className={"download"}>
         <a
-          href="https://github.com/MeloScav/cv-portfolio/raw/master/CV-SCAVEZZONI-Meloe.pdf"
+          href="https://github.com/MeloScav/cv-portfolio/raw/master/CV-SCAVEZZONI-Meloe-FR.pdf"
           download="CV-SCAVEZZONI-Meloe"
         >
           <button type={"button"}>{"Download the CV in pdf"} </button>
